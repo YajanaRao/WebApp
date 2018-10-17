355e14a7bc09c8ec039d7a8ced0d058c80d8466c
YajanaRao <yajananrao@gmail.com>
2018-10-17 10:53:37 +0530

    Update

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,15 @@
 import os
-
 from flask import Flask
 from flask import render_template
+from flask_assets import Environment, Bundle
+
 def create_app():
     app = Flask(__name__)
     # existing code omitted
+    assets = Environment(app)
+    assets.url = app.static_url_path
+    scss = Bundle('assets\scss\style.scss', filters='pyscss', output='css\style.css')
+    assets.register('scss_all', scss)
 
     app.config.from_mapping(
         SECRET_KEY='dev',
